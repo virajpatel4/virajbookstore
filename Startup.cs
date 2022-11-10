e3779f272aa9470f6ac2157277f7c577080c5c90
virajpatel4 <VP97@myscc.ca>
2022-11-10 16:41:51 -0500

    added readme

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using virajbookstore.Data;
+using virajbookstore.DataAccess.Data;
 
 namespace virajbookstore
 {
