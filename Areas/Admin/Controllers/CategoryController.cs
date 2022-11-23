a2a64deab700b24a71aa0178ec14aa51449cca52
virajpatel4 <VP97@myscc.ca>
2022-11-23 18:55:20 -0500

    Trying to solve category page error

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using virajbooks.DataAccess.Repository.IRepository;
+using virajbooks.DataAccess.Repository;
+using virajbookstore.DataAccess;
+using virajbookstore.Areas;
+using virajbookstore.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using virajbooks.DataAccess.Repository.IRepository;
 using virajbooks.Models;
 
 namespace virajbookstore.Areas.Admin.Controllers
 {
     [Area("Admin")]
+
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -23,47 +28,55 @@ namespace virajbookstore.Areas.Admin.Controllers
             return View();
         }
 
-        public IActionResult Upsert(int? id)
+
+        public IActionResult Upsert(int? id)        // action method for upsert
         {
-            Category category = new Category();
+            Category category = new Category();    
+
             if (id == null)
             {
+                //this is for create
                 return View(category);
             }
+
+            //this is for edit
             category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+
             if (category == null)
             {
                 return NotFound();
             }
-            // return View(category);
-            return View();
+
+            return View(category);
         }
 
-        //// use HTTP POST to define the post-action method
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Upsert(Category category)
-        //{
-        //    if (ModelState.IsValid) //checks all validations in the model (e.g. Name required) to increase security
-        //    {
-        //        if (category.Id == 0)
-        //        {
-        //            _unitOfWork.Category.Add(category);
 
-        //        }
-        //        else
-        //        {
-        //            _unitOfWork.Category.Update(category);
-        //        }
-        //        _unitOfWork.Save();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(category);
-        //}
+        // use HTTP POST to define the post-action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)   // check all variables in the model (e.g. Name Required) to increase security
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                    //_unitOfWork.save();
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));     // to see all the categories
+            }
+
+            return View(category);
+        }
 
 
-        // API calls here
-
+        // API calls here for Delete
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
@@ -73,18 +86,19 @@ namespace virajbookstore.Areas.Admin.Controllers
             return Json(new { data = allObj });
         }
 
-        //[HttpDelete]
-        //public IActionResult Delete(int id)
-        //{
-        //    var objFromDb = _unitOfWork.Category.Get(id);
-        //    if (objFromDb == null)
-        //    {
-        //        return Json(new { success = false, message = "Error while deleting" });
-        //    }
-        //    _unitOfWork.Category.Remove(objFromDb);
-        //    _unitOfWork.Save();
-        //    return Json(new { success = true, message = "Delete Successful" });
-        //}
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successfully" });
+        }
         #endregion
     }
 }
